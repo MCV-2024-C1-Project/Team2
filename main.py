3be2dfb4da0ffa71457ee413646f541b83aaf031
Vince100599 <vheuer@icloud.com>
2024-10-03 13:03:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import cv2
 import matplotlib.pyplot as plt
 
+# ---------------------------------------------------------------------------------
+# Author: Agustina Ghelfi, Grigor Grigoryan, Philip Zetterberg, Vincent Heuer
+# Date: 03.10.2024
+# Version: 1.0
+# 
+# Version 1.1 ads more detailed German descriptions to the notebook
+#
+# Description:
+# This Python script is designed to analyze part lists of wooden crates (HPE 5 tons) in order to calculate manufacturing times.
+#
+#
+# In the future this should be checked with more data from different production locations of Axxum. Furthermore, representation of 
+# bottom boards need to be included aswell as a dynamic representation of OSB boards in the sawing process.
+# ---------------------------------------------------------------------------------
+
+
+
+
 
 img = cv2.imread('data/BBDD/bbdd_00000.jpg', cv2.IMREAD_GRAYSCALE)
 hist = cv2.calcHist([img], [0], None, [256], [0, 256])
