ba741b2771638e4f108c04c1a6cd2ea4227fe4f8
Vince100599 <vheuer@icloud.com>
2024-10-03 13:17:57 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,6 +21,7 @@ def euc_dist(h1, h2):
 
     return distance   
 
+
 def L1_dist(h1, h2):
     
     if len(h1) != 256 or len(h2) != 256:
@@ -32,7 +33,8 @@ def L1_dist(h1, h2):
     distance = np.sum(np.abs(h1 - h2))
     
     return distance
-    
+
+
 def X2_distance(h1, h2):
 
     if len(h1) != 256 or len(h2) != 256:
@@ -45,6 +47,7 @@ def X2_distance(h1, h2):
 
     return distance
 
+
 def histogram_similiarity(h1, h2):
 
     if len(h1) != 256 or len(h2) != 256:
@@ -56,13 +59,20 @@ def histogram_similiarity(h1, h2):
     similiarity = np.sum(np.minimum(h1, h2))
     return similiarity
 
-def hellinger_kernel(h1, h2):
 
+def hellinger_kernel(h1, h2):
+    
     if len(h1) != 256 or len(h2) != 256:
         raise ValueError("Both histograms must have a length of 256")
-    
+        
     h1 = np.array(h1)
     h2 = np.array(h2)
     
     similiarity = np.sum(np.sqrt(h1*h2))
     return similiarity
+
+def load_and_print_pkl(pkl_file_path):
+    with open(pkl_file_path, 'rb') as pkl_file:
+        data = pickle.load(pkl_file)      
+        print("Content of the pickle file:")
+        print(data)
