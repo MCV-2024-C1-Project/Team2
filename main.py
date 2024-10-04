590387529c3bac40229f0e459e85ee9248384898
Vince100599 <vheuer@icloud.com>
2024-10-04 11:38:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,24 +20,43 @@ import utils
 # In the future this mainscript is expanded to enhance the system's capability and to tackle the upcoming challenges.
 # ---------------------------------------------------------------------------------
 
-
-
-
-
-img = cv2.imread('data/BBDD/bbdd_00000.jpg', cv2.IMREAD_GRAYSCALE)
-hist = cv2.calcHist([img], [0], None, [256], [0, 256])
-plt.plot(hist, color='black')
-plt.title('Histograma')
-plt.xlabel('Intensidad de píxel')
-plt.ylabel('Frecuencia')
-plt.xlim([0, 256])  # Asegurarse de que el eje X cubra el rango de 0 a 255
-plt.show()
-
-# color
-chans = cv2.split(img)
-colors = ("b", "g", "r")
-
-
-
 #----------<main>-----------
+#----------<task 1>---------
 
+directory = 'data/BBDD'
+
+for filename in os.listdir(directory):
+    if filename.endswith('.jpg'):
+        img_path = os.path.join(directory, filename)
+
+        img_grey = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        hist_grey = cv2.calcHist([img_grey], [0], None, [256], [0, 256])
+        hist_grey /= hist_grey.sum() # changed "hist.sum() to hist_grey.sum()"
+
+
+        # RGB
+        img_RGB=cv2.imread(img_path)
+        B, G, R = cv2.split(img_RGB)
+
+        hist_B = cv2.calcHist([B], [0], None, [256], [0, 256])
+        hist_B /= hist_R.sum()
+
+        hist_G = cv2.calcHist([G], [0], None, [256], [0, 256])
+        hist_G /= hist_G.sum()
+
+        hist_R = cv2.calcHist([R], [0], None, [256], [0, 256])
+        hist_R /= hist_B.sum()
+
+
+        histograms = {
+            'grey': hist_grey,
+            'hist_B': hist_B,
+            'hist_G': hist_G,
+            'hist_R': hist_R,
+        }
+
+        pkl_filename = os.path.splitext(filename)[0] + '.pkl'
+        pkl_path = os.path.join(directory, pkl_filename)
+
+        with open(pkl_path, 'wb') as pkl_file:
+            pickle.dump(histograms, pkl_file)
