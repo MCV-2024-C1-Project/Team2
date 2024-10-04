f8055a68d058ac589ff89649aa7fa41a9d3b83f9
Vince100599 <vheuer@icloud.com>
2024-10-04 10:41:03 +0200

    Update utils.py with apk and makp from benhammer

--- utils.py
+++ utils.py
@@ -132,7 +132,46 @@ def load_and_print_pkl(pkl_file_path):
         data = pickle.load(pkl_file)      
         print("Content of the pickle file:")
         print(data)
-        
+
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+    Source: https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
+    This function computes the average prescision at k between two lists of
+    items.
+
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+
+    """
+    if len(predicted)>k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+
 def mapk(actual, predicted, k=10):
     """
     source: https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
